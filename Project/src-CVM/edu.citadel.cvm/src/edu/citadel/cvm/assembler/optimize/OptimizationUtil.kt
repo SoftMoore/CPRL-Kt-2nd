9573adc19cbcaf6a0bc25b10945d0b6c32a18a8b
SoftMoore <jmoore@softmoore.com>
2023-05-22 06:36:12 -0400

    cleaned up comment for function getShiftAmount()

--- Project/src-CVM/edu.citadel.cvm/src/edu/citadel/cvm/assembler/optimize/OptimizationUtil.kt
+++ Project/src-CVM/edu.citadel.cvm/src/edu/citadel/cvm/assembler/optimize/OptimizationUtil.kt
@@ -1,17 +1,13 @@
 package edu.citadel.cvm.assembler.optimize
 
-/*
- * Utility for shift-related optimizations.
- */
-
 /**
- * If n is a power of 2, returns log2(n) (i.e., returns the exponent);
- * otherwise returns 0.
+ * Utility for shift-related optimizations.  If n is a power of 2,
+ * returns log2(n) (i.e., returns the exponent); otherwise returns 0.
  */
 fun getShiftAmount(n : Int) : Byte
   {
     var n1 = n
-    var result: Byte = 0
+    var result : Byte = 0
 
     while (n1 > 1)
       {
