ced92cf57fa724ef171a63b05fb59db22a9f6cd8
SoftMoore <jmoore@softmoore.com>
2023-06-05 07:18:50 -0400

    minor changes to comments and formatting

--- Project/src-ParserV3/edu.citadel.cprl/src/edu/citadel/cprl/ast/NotExpr.kt
+++ Project/src-ParserV3/edu.citadel.cprl/src/edu/citadel/cprl/ast/NotExpr.kt
@@ -10,18 +10,14 @@ import edu.citadel.cprl.Type
  * The abstract syntax tree node for a not expression.  A not expression is a unary
  * expression of the form "not expr".  A simple example would be "not isEmpty()".
  *
- * @constructor Construct a not expression with the specified operator ("not")
- *              and operand.
+ * @constructor Construct a not expression with the specified operator ("not") and operand.
  */
 class NotExpr(operator : Token, operand : Expression) : UnaryExpr(operator, operand)
   {
-    /**
-     * Initialize the type of the expression to Boolean.
-     */
     init
       {
         assert(operator.symbol == Symbol.notRW)
-            { "Operator is not the reserved word \"not\"." }
+          { "Operator is not the reserved word \"not\"." }
         type = Type.Boolean
       }
 
